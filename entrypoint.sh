b016eb8cba0c237c46fa6057f57618f9249eaa97
Pavel Bukhatkin <welderpb@users.noreply.github.com>
2025-03-01 08:03:14 +0100

    add user group

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ do
     IFS=':' read -r user uid pass <<EOF
 $USER
 EOF
-
-    adduser -u ${uid} -G users -H -h /var/empty -D -s /sbin/nologin ${user}
+    addgroup -g ${uid} ${user}
+    adduser -u ${uid} -G ${user} -G users -H -h /var/empty -D -s /sbin/nologin ${user}
     echo "${pass}" | tee - | smbpasswd -s -c /etc/samba/smb.conf -a ${user}
 done
 
